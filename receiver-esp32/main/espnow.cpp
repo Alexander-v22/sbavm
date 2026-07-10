695e4e2d9f0eef58cb22724f994d440c41617224
Alexander-v22 <av275@duke.edu>
2026-07-10 00:24:23 -0400

    add dashboard: index.html, style.css, app.js with websocket connection

--- receiver-esp32/main/espnow.cpp
+++ receiver-esp32/main/espnow.cpp
@@ -1,20 +1,44 @@
 #include "espnow.h"
 #include "esp_log.h"
 #include "esp_mac.h"
+#include "websocket.h"
+#include <stdio.h>
+
+
 
 static const char *TAG = "ESPNOW_RX";
 
-static void espnow_recv_callback(const esp_now_recv_info_t *info, const uint8_t *data, int len){
+static const uint8_t ankle_mac[6] = {0xac, 0x27, 0x6e, 0x7e, 0x19, 0x1c};
+static const uint8_t waist_mac[6] = {0xac, 0x27, 0x6e, 0x7e, 0x20, 0x24};
+static const uint8_t wrist_mac[6] = {0xac, 0x27, 0x6e, 0x7e, 0xa3, 0x8c};
 
-    if(len != sizeof(espnow_packet_t)) {
+static const char* mac_to_node_name(const uint8_t *mac) {
+    if (memcmp(mac, ankle_mac, 6) == 0) return "ankle";
+    if (memcmp(mac, waist_mac, 6) == 0) return "waist";
+    if (memcmp(mac, wrist_mac, 6) == 0) return "wrist";
+    return "unknown";
+}
+
+static void espnow_recv_callback(const esp_now_recv_info_t *info, const uint8_t *data, int len) {
+  
+    if (len != sizeof(espnow_packet_t)) {
         ESP_LOGE(TAG, "Invalid packet size");
         return;
-    } 
-
-    espnow_packet_t packet; //empty struct created in local memory
+    }
+    
+    espnow_packet_t packet;
     memcpy(&packet, data, sizeof(espnow_packet_t));
     
-    ESP_LOGI(TAG,"MAC:%02x gyro_x:%.2f ts:%lld", packet.mac[0], packet.gyro_x, packet.timestamp);
+    const char *node_name = mac_to_node_name(packet.mac);
+    
+    char json[200];
+    snprintf(json, sizeof(json),
+             "{\"type\":\"imu\",\"node\":\"%s\",\"gyro_x\":%.2f,\"gyro_y\":%.2f,\"gyro_z\":%.2f,\"timestamp\":%lld,\"peak\":false}",
+             node_name, packet.gyro_x, packet.gyro_y, packet.gyro_z, packet.timestamp);
+    
+    websocket_send(json);
+    
+    ESP_LOGI(TAG, "%s", json);
 }
 
 esp_err_t espnow_init(void) {
@@ -31,3 +55,4 @@ esp_err_t espnow_init(void) {
     return ESP_OK;
 
 }
+
